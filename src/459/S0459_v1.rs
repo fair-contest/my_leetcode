f4bd17c5b32acfab9abddfeab105050cd3617060
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-05-13 13:38:04 +0800

    Create S0459_v1.rs

---
+++ src/459/S0459_v1.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn repeated_substring_pattern(s: String) -> bool {
+        let n: i32 = s.as_bytes().into_iter().map(|&x| (x - 96) as i32).sum();
+        let mut prev = 0;
+        for i in 1..(&s.len()/2+1) {
+            prev += (&s.as_bytes()[i] - 96) as i32;
+            if &s.len() % i == 0 && &n % &prev == 0 {
+                let mut x = i.clone();
+                while &x != &s.len() {
+                    if &s[0..i] != &s[x..x+&i] { break; }
+                    x += &i;
+                }
+                if x == s.len() { return true; }
+            }
+        }
+        false
+    }
+}
