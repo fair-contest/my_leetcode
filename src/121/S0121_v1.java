2b815e3a2fcda2a21d2141a88ddbf238855aeba9
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-08-27 01:03:09 +0800

    Update S0121_v1.java

--- src/121/S0121_v1.java
+++ src/121/S0121_v1.java
@@ -2,13 +2,15 @@ class Solution {
     public int maxProfit(int[] prices) {
         int maxp = prices[0];
         int minp = prices[0];
+        int res = 0;
         for(int i=0;i<prices.length;i++) {
             if (prices[i] < minp) {
                 minp = prices[i];
                 maxp = prices[i];
             }
             if (prices[i] > maxp) { maxp = prices[i]; }
+            res = Math.max((maxp - minp), res);
         }
-        return ((maxp - minp) > 0) ? maxp - minp : 0;
+        return res;
     }
 }
