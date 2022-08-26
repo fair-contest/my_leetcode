040c866d4e62f6f51912730b9ea4d9a45c1564c7
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-08-27 00:57:54 +0800

    Create S0121_v1.java

---
+++ src/121/S0121_v1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxp = prices[0];
+        int minp = prices[0];
+        for(int i=0;i<prices.length;i++) {
+            if (prices[i] < minp) {
+                minp = prices[i];
+                maxp = prices[i];
+            }
+            if (prices[i] > maxp) { maxp = prices[i]; }
+        }
+        return ((maxp - minp) > 0) ? maxp - minp : 0;
+    }
+}
