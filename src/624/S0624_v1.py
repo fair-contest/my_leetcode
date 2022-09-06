f2c260219bbf74ba72265aabd43cc4faa828b4d6
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-09-07 01:29:29 +0800

    Delete S0624_v1.py

--- src/624/S0624_v1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findLongestChain(self, pairs: List[List[int]]) -> int:
-        return sum(self.gen(sorted(pairs, key=lambda x: x[1]), -inf))
-
-    def gen(self, list, val):
-        for a, b in list:
-            if val < a:
-                val = b
-                yield 1
