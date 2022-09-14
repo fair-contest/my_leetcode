84249348b94994da0902d0c4df2d8b87b9edba90
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-09-14 14:10:47 +0800

    Update S1619_v1.rs

--- src/1619/S1619_v1.rs
+++ src/1619/S1619_v1.rs
@@ -1,9 +1,12 @@
+use std::collections::BinaryHeap;
+
 impl Solution {
     pub fn trim_mean(arr: Vec<i32>) -> f64 {
         let r = arr.len() / 20;
-        let mut heap_min = std::collections::BinaryHeap::with_capacity(r+1);
-        let mut heap_max = std::collections::BinaryHeap::with_capacity(r+1);
+        let mut heap_min = BinaryHeap::with_capacity(r+1);
+        let mut heap_max = BinaryHeap::with_capacity(r+1);
         let mut i = 0;
+        let mut j = 0;
         let mut tmp = 0;
         let mut sum = 0;
         while i < 2 * r {
@@ -11,13 +14,23 @@ impl Solution {
             heap_max.push(-1 * arr[i+1]);
             i += 2;
         }
+        tmp = Solution::ex_heap(&mut heap_max, &mut heap_min, arr[i]);
+        while j < r-1 {
+            tmp = Solution::ex_heap(&mut heap_max, &mut heap_min, tmp);
+            j += 1;
+        }
+        sum = tmp;
+        i += 1;
         while i < arr.len() {
-            heap_min.push(arr[i]);
-            tmp = heap_min.pop().unwrap();
-            heap_max.push(-1 * tmp);
-            sum += heap_max.pop().unwrap() * -1;
+            sum += Solution::ex_heap(&mut heap_max, &mut heap_min, arr[i]);
             i += 1;
         }
         sum as f64 / (arr.len() - r * 2) as f64
+    }
+
+    fn ex_heap(h_max: &mut BinaryHeap<i32>, h_min: &mut BinaryHeap<i32>, v: i32) -> i32 {
+        h_min.push(v);
+        h_max.push(-1 * h_min.pop().unwrap());
+        h_max.pop().unwrap() * -1
     }
 }
