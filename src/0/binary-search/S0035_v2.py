4fd4bb0984391ae3dbba7d8961fc0ebc62c17cae
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-10-15 18:01:56 +0800

    Create S0035_v2.py

---
+++ src/0/binary-search/S0035_v2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums)-1
+        while start <= end:
+            i = start + (end - start) // 2
+            if target > nums[i]:
+                start = i + 1
+            elif target < nums[i]:
+                end = i - 1
+            else:
+                return i
+        return start
