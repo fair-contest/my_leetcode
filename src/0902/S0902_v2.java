edc33d9f76d715549d2a8863929568154e7cd480
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-10-18 18:17:14 +0800

    Create S0902_v2.java

---
+++ src/0902/S0902_v2.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int atMostNGivenDigitSet(String[] digits, int n) {
+        int[] nums = nToArray(n);
+        int nlen = nums.length;
+        int dlen = digits.length;
+        int res = 0;
+        int[] plist = new int[nlen];
+        plist[nlen-1] = 1;
+        if (nlen > 1) {
+            plist[nlen-2] = dlen;
+            res += dlen;
+            if (nlen > 2) {
+                for (int i=nlen-3;i>=0;i--) {
+                //题目digits和n长度不超9，所以下面这行代码计算范围最大也只是9的8次方，不会溢出。
+                    plist[i] = plist[i+1] * dlen;
+                    res += plist[i];
+                }
+            }
+        }
+        int i = 0, check = 0, opt= 0, cur = 0;
+        for (i=0;i<nlen;i++) {
+            check = 0; opt = 0;
+            cur = nums[i];
+            for (String s : digits) {
+                int tmp = Integer.valueOf(s);
+                if (tmp <= cur) check++;
+                if (tmp < cur) opt++;
+            }
+            if (check == 0) break;
+            res += plist[i] * opt;
+            if (check == opt) break;
+        }
+        return (i == nlen && check != 0) ? res + check - opt : res;
+    }
+
+    private int[] nToArray(int n) {
+        int[] arr = new int[9];
+        int i = 0;
+        while (n > 0) {
+            arr[i] = n % 10;
+            n /= 10;
+            i++;
+        }
+        int[] res = new int[i];
+        for (int j = 0; j < i; j++) {
+            res[j] = arr[i-j-1];
+        }
+        arr = null;
+        return res;
+    }
+}
