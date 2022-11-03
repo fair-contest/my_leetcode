ae18909bd8a2e0b6fe2c05d9ab9305b88a708ef3
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-11-03 15:13:20 +0800

    Create S1668_v1.c

---
+++ src/1668/S1668_v1.c
@@ -0,0 +1,33 @@
+bool cmpWord(char* p, int startp, int endp, char* q) {
+    for (int i = 0; i < endp-startp; ++i) {
+        if (p[startp+i] != q[i]) return false;
+    }
+    return true;
+}
+
+int maxRepeating(char * sequence, char * word){
+    int m = strlen(sequence), n = strlen(word);
+    if (m < n) return 0;
+    int res = 0;
+    int i = n, j = m - n + 1, cur = 0;
+    for (int k = 0;k < j;++k) {
+        if (cmpWord(sequence, k, n+k, word)) {
+            res = 1;
+            break;
+        }
+    }
+    while (i < j) {
+        if (cmpWord(sequence, i-n, i, word)) cur = 1;
+        else cur = 0;
+        if (cmpWord(sequence, i, i+n, word)) {
+            cur++;
+            i += n;
+            while ((i < j) && (cmpWord(sequence, i, i+n, word))) {
+                cur++;
+                i += n;
+            }
+        } else i++;
+        if (cur > res) res = cur;
+    }
+    return res;
+}
