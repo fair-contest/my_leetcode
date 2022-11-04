faf910cbefd88a378a24dfc654382989ab104c5e
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-11-05 06:11:30 +0800

    Update S0754_v1.py

--- src/0754/S0754_v1.py
+++ src/0754/S0754_v1.py
@@ -1,3 +1,3 @@
 class Solution:
     def reachNumber(self, target: int) -> int:
-        return n if (t:=abs(target)) == (s:=(n:=round((2*t)**0.5))*(n+1)>>1) or s-t&1 == 0 else n+1+(n&1)
+        return n if (t:=abs(target)) == (s:=(n:=round((t<<1)**0.5))*(n+1)>>1) or s-t&1 == 0 else n+1+(n&1)
