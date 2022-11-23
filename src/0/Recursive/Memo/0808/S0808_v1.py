db9b6c5bd1e39d1a085b9b86f910bb5b5aed3fee
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-11-23 10:04:01 +0800

    Update S0808_v1.py

--- src/0/Recursive/Memo/0808/S0808_v1.py
+++ src/0/Recursive/Memo/0808/S0808_v1.py
@@ -1,13 +1,16 @@
 class Solution:
     def soupServings(self, n: int) -> float:
-        if n > 4275:
-            return 0.99999
+        if n > 4275: return 0.99999
         n = n * 0.01
         @cache
         def dfs(a: float, b: float) -> float:
-            if a <= 0 and b <= 0: return 0.5
-            elif a <= 0: return 1
-            elif b <= 0: return 0
+            if a <= 0:
+                if b <= 0:
+                    return 0.5
+                else:
+                    return 1.0
+            elif b <= 0:
+                return 0.0
             return 0.25 * (dfs(a-1, b) + dfs(a-0.75, b-0.25) + dfs(a-0.5, b-0.5) + dfs(a-0.25, b-0.75))
 
         return dfs(n, n)
