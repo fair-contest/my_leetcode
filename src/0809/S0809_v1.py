f1c3c8cd5339b1f0729ff155533c94207714bdf7
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-11-25 12:38:34 +0800

    Update S0809_v1.py

--- src/0809/S0809_v1.py
+++ src/0809/S0809_v1.py
@@ -1,3 +1,4 @@
 class Solution:
     def expressiveWords(self, s: str, words: List[str]) -> int:
-        return sum(1 for i in words if re.compile("("+"{3,100})(".join((c+'?|'+c for c in i)) + "{3,100})$").match(s))
+        #return sum(1 for i in words if re.compile("("+"{3,100})(".join((c+'?|'+c for c in i)) + "{3,100})$").match(s))
+        return sum(1 for i in words if re.match("".join(('('+c+'|'+c+'{3,100})('+c+'(?='+c+'))?' for c in i)) + "$", s))
