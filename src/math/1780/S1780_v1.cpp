9255e53439668f9bc283ec3c84088ebaa67631e0
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-12-09 15:53:10 +0800

    Create S1780_v1.cpp

---
+++ src/math/1780/S1780_v1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int tb[15] = {4782969, 1594323, 531441, 177147, 59049, 19683, 6561, 2187, 729, 243, 81, 27, 9, 3, 1};
+    bool checkPowersOfThree(int n) {
+        for (int i = 0; i < 15; ++i) {
+            if (n >= tb[i]) {
+                if (tb[i] == n) break;
+                if (1 + ((3 * tb[i] - 3) >> 1) < n) return false;
+                n -= tb[i];
+            }
+        }
+        return true;
+    }
+};
