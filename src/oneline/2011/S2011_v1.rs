84841f2f9971f53c340ac1dd600a78f361d6a9f2
fair-contest <45393745+fair-contest@users.noreply.github.com>
2022-12-23 20:08:02 +0800

    Update S2011_v1.rs

--- src/oneline/2011/S2011_v1.rs
+++ src/oneline/2011/S2011_v1.rs
@@ -1,5 +1,6 @@
 impl Solution {
     pub fn final_value_after_operations(operations: Vec<String>) -> i32 {
         operations.iter().fold(0, |x, y|if y.contains("+") {x + 1} else {x - 1})
+        // operations.iter().map(|x|if x.contains("+") {1} else {-1}).sum()
     }
 }
